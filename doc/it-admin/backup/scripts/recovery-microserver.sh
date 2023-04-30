825dcc05690efba09464f4c0c116b5127d7c3967
Bouarfa Mahi <63168407+BouarfaMahi@users.noreply.github.com>
2023-04-30 15:02:16 +0200

    Update recovery-microserver.sh

--- doc/it-admin/backup/scripts/recovery-microserver.sh
+++ doc/it-admin/backup/scripts/recovery-microserver.sh
@@ -3,5 +3,6 @@
 #!/bin/bash
 
 rm -rf /home /opt
-cp -pr /backup/rsnapshot/daily.1/localhost/home /
-cp -pr /backup/rsnapshot/daily.1/localhost/opt /
+cp -pr /backup/rsnapshot/hourly.0/localhost/home /
+cp -pr /backup/rsnapshot/hourly.0/localhost/opt /
+updatedb
